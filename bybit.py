9c34fbdf0c3305d4728f4a27b0e24384bea52019
Aziz Zeghal <101135665+Aziz-Zeghal@users.noreply.github.com>
2023-12-27 17:28:06 +0100

    Cleaning take arbitrage and adding info to holdtime

--- bybit.py
+++ bybit.py
@@ -14,11 +14,10 @@ else:
 # All requests will return a dictionnary containing retCode, retMsg, result, retExtInfo, time
 # In our case, inside result we have the category, and the list of pairs
 # TODO: All functions make requests with API, handle all possible errors while executing code
-# TODO: Add a parameter to make functions verbose
 
 def getSymbols():
     """
-    Prints the symbol and funding rate for each pair in Perpetual market.
+    Prints the symbol and APY with funding rate for each pair in Perpetual market.
 
     Args:
         None
@@ -27,8 +26,12 @@ def getSymbols():
     """
     pairs = session.get_tickers(category="linear")
     for p in pairs['result']['list']:
-        print(p['symbol'] + " with " + p['fundingRate'])
-        
+        if p['fundingRate'] != '':
+            apy = round(float(p['fundingRate']) * 365 * 3 * 100, 2)
+        else:
+            apy = 0
+        print(p['symbol'] +" with an APY of " + str(apy))
+
 def getWallet():
     """
     Prints the total value of the wallet.
@@ -55,6 +58,7 @@ def getCoinBalance(coin):
     coinsum = session.get_wallet_balance(accountType="UNIFIED", coin=coin)
     print("Total value of this coin is: " + coinsum['result']['list'][0]['coin'][0]['usdValue'] + "💲")
 
+# This function is kinda useless now
 def getFundingRate(coin):
     """
     Prints the funding rate and APY of a given coin.
@@ -67,11 +71,51 @@ def getFundingRate(coin):
     try:
         pair = session.get_tickers(category="linear", symbol=coin)['result']['list'][0]
         value = float(pair['fundingRate']) * 100
-        apy = float(pair['fundingRate']) * 365 * 3 * 100
         print(coin + " has a funding rate of " + str(value))
         print("This coin brings an APY of " + str(apy) + "%!")
     except:
         print("Coin does not exist in this mode!")
+
+# TODO: This function takes ONLY coins that exist in spot and perpetual
+# TODO: If condition to check coin exists and in good format, for user's confort
+def holdTime(coin, amount=100):
+    """
+    Prints the total fees,
+    the required percentage to break even, 
+    the funding rate
+    and how many funding rates have to be perceived to be in profit.
+    Gets the fees from get_fee_rates(dynamic), and the funding rate from get_tickers.
+
+    Args:
+        coin (string)
+        amount (int)
+    Returns:
+        totalFees (float)
+        breakEven (float)
+    """
+    spotcoin = session.get_fee_rates(category="spot", symbol=coin)['result']['list'][0]
+    perpcoin = session.get_fee_rates(category="linear", symbol=coin)['result']['list'][0]
+    
+    spotMaker, spotTaker = float(spotcoin['makerFeeRate']) * amount, float(spotcoin['takerFeeRate']) * amount
+    perpMaker, perpTaker = float(perpcoin['makerFeeRate']) * amount, float(perpcoin['takerFeeRate']) * amount
+    
+    totalFees = spotMaker + spotTaker + perpMaker + perpTaker
+    fundingRate = float(session.get_tickers(category="linear", symbol=coin)['result']['list'][0]['fundingRate']) * 100
+    breakEven = round(totalFees * 100 / amount, 5)
+    fundingRateTakes = round(breakEven / fundingRate, 0)
+    apy = fundingRate * 365 * 3
+
+    print("P: " + str(perpMaker) + " and exit: " + str(perpTaker))
+    print("S: " + str(spotMaker) + " and exit: " + str(spotTaker))
+    print("Total fees: " + str(totalFees) + "$\n")
+    
+    print(str(amount * fundingRate / 100) + "$ every pay")
+    print("So an APY of " + str(apy) + "%\n")
+    
+    print("Required to break even: " + str(breakEven) + "%")
+    print("With funding rates of: " + str(fundingRate) + "%")
+    print("How many funding rates have to be perceived: " + str(fundingRateTakes))
+    return totalFees, breakEven, apy
 
 # TODO: Not optimal at all, O(n^2) at worst
 def bestFundingRate():
@@ -95,42 +139,6 @@ def bestFundingRate():
     print("This coin brings an APY of " + str(apy) + "%!")
     return coin
 
-# TODO: This function takes ONLY coins that exist in spot and perpetual
-# TODO: If condition to check coin exists and in good format, for user's confort
-def holdTime(coin, amount=100):
-    """
-    Prints the total fees,
-    the required percentage to break even, 
-    the funding rate
-    and how many funding rates have to be perceived to be in profit.
-    Gets the fees from get_fee_rates(dynamic), and the funding rate from get_tickers.
-
-    Args:
-        coin (dict)
-        amount (int)
-    Returns:
-        totalFees (float)
-        breakEven (float)
-    """
-    spotcoin = session.get_fee_rates(category="spot", symbol=coin)['result']['list'][0]
-    perpcoin = session.get_fee_rates(category="linear", symbol=coin)['result']['list'][0]
-    
-    spotMaker, spotTaker = float(spotcoin['makerFeeRate']) * amount, float(spotcoin['takerFeeRate']) * amount
-    perpMaker, perpTaker = float(perpcoin['makerFeeRate']) * amount, float(perpcoin['takerFeeRate']) * amount
-    
-    totalFees = spotMaker + spotTaker + perpMaker + perpTaker
-    fundingRate = float(session.get_tickers(category="linear", symbol=coin)['result']['list'][0]['fundingRate']) * 100
-    breakEven = round(totalFees * 100 / amount, 5)
-    fundingRateTakes = round(breakEven / fundingRate, 0)
-    print("Perpetual enter: " + str(perpMaker) + " and exit: " + str(perpTaker))
-    print("Spot enter: " + str(spotMaker) + " and exit: " + str(spotTaker))
-
-    print("Total fees: " + str(totalFees) + "$")
-    print("Required to break even: " + str(breakEven) + "%")
-    print("With funding rates of: " + str(fundingRate) + "%")
-    print("How many funding rates have to be perceived: " + str(fundingRateTakes))
-    return totalFees, breakEven
-
 # TODO: Use threads to execute jobs simultanous
 # TODO: Use sockets to retrieve live time information
 # TODO: Add a small verification of the minOrderQty with get_instruments_info
@@ -148,13 +156,16 @@ def enterArbitrage(coin, amount=20):
     """
     spot = session.get_tickers(category="spot", symbol=coin['symbol'])['result']['list'][0]
     perp = session.get_tickers(category="linear", symbol=coin['symbol'])['result']['list'][0]
-    session.set_leverage(category="linear", symbol=coin['symbol'], buyLeverage="1", sellLeverage="1")
+    try:
+        session.set_leverage(category="linear", symbol=coin['symbol'], buyLeverage="1", sellLeverage="1")
+    except:
+        print("Already set to 1x")
     while spot['lastPrice'] != perp['lastPrice']:
         print("spot " + spot['lastPrice'] + " and perp " + perp['lastPrice'])
         time.sleep(0.2)
         spot = session.get_tickers(category="spot", symbol=coin['symbol'])['result']['list'][0]
         perp = session.get_tickers(category="linear", symbol=coin['symbol'])['result']['list'][0]
-    quantity = str(round(amount / float(spot['lastPrice']), 1))
+    quantity = str(int(amount / float(spot['lastPrice'])))
     session.place_order(
         category="spot",
         symbol=coin['symbol'],
@@ -170,25 +181,21 @@ def enterArbitrage(coin, amount=20):
         qty=quantity,
         price=spot['lastPrice'])
 
-# TODO: getPNLPerpetual(coin)
-# Takes the perpetual contract of a coin, calculates the PNL - holdTime
-
 # TODO: exitArbitrage(coin), with a given coin, exit both positions
 # No conditions, just exit it.
 
 # TODO: Takedecision()
 # If fundingRate is negative, exit
-# If losing, or if APY < to a certain required amount (50% for example), exit
+# If APY < to a certain required amount (50% for example), exit (if fees are covered)
 # Passes everything ? Keep it
-# Always return the holdTime info with the right amount, if it was removed, getPNLPerpetual(coin)
-# (totalfees, breakEven, APY, PNL, removed)
+# Always return the holdTime info with the right amount, and if it was removed
 
 # TODO: cleanPositions()
 # Will check all current position, use Takedecision()
-# Take information of it to do stats (the position was exited/kept, APY with getFundingRate, PNL is positive/negative)
+# Take information of it to do stats (the position was exited/kept, APY, PNL is positive/negative)
 
 # theUltimate
 # Check if positions are open, use cleanPosition
-# Checks available USDT and use while balance > 20 and margin < 80 %
+# Checks available USDT and use while balance > 20
 # Get the best pair use enterArbitrage(bestFundingRate)
 # Every 8 hours, use cleanPosition()
