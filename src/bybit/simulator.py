a3eef70dca8c3849e4b3e8b9cd008e9c5e167ad8
Aziz Zeghal <aziz.zeghal@gmail.com>
2025-02-18 20:08:03 +0000

    feature: sub_fundings added

--- src/bybit/simulator.py
+++ src/bybit/simulator.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
@@ -21,9 +22,13 @@ class Simulator:
                 contract: load_klines_parquet(contract),
             }
 
-    def plot_candles(
-        self, contract: str, lowerlimit: str = "2024-01-01 00:00", upperlimit: str = "2026-01-01 00:00"
-    ) -> dict:
+    def to_graph(
+        self,
+        contract: str,
+        lowerlimit: str = "2024-01-01 00:00",
+        upperlimit: str = "2026-01-01 00:00",
+        onlyData: bool = False,
+    ) -> dict | pd.DataFrame:
         """Take a contract, transforms it to a pandas DataFrame, and plots it as a candlestick chart.
 
         The contract contains a list of candles in the format:
@@ -34,6 +39,7 @@ class Simulator:
             contract (str): The contract containing the candles data
             lowerlimit (str): The lower bound date to filter the data (format: YYYY-MM-DD HH:MM)
             upperlimit (str): The upper bound date to filter the data (format: YYYY-MM-DD HH:MM)
+            onlyData (bool): If True, will only return the clean DataFrame
 
         Returns:
             dict: {"figure": fig, "dataframe": df}
@@ -47,6 +53,9 @@ class Simulator:
         # Filter according to the date
         # This syntax looks like numpy
         df = df[(df["startTime"] >= lowerlimit) & (df["startTime"] <= upperlimit)]
+
+        if onlyData is True:
+            return df
 
         # Use plotly
         fig = go.Figure(
@@ -81,17 +90,72 @@ class Simulator:
                 # Y-axis extension and more granular tick intervals
                 yaxis={"range": [y_min, y_max], "tickmode": "linear", "dtick": (y_max - y_min) / 10},
             )
-        except Exception as e:
+        except Exception:
             print("Are you sure the date limits are correct?")
-            raise e
+            raise
         return fig, df
 
-    def plot_compare(
+    def sub_fundings(self, df_contract: pd.DataFrame) -> tuple[go.Scatter, go.Scatter]:
+        """Draw the cumulated funding of a contract.
+
+        WARNING: The DataFrame should contain the 'fundingRate' column.
+        WARNING2: For now, mostly works for 1-minute candles.
+        """
+        # Convert startTime to datetime if it's not already
+        df_contract["startTime"] = pd.to_datetime(df_contract["startTime"])
+
+        # Funding rate trace
+        funding_trace = go.Scatter(x=df_contract["startTime"], y=df_contract["fundingRate"], name="FundingRate")
+
+        # Filter timestamps closest to 00:59, 08:59, and 16:59
+        filtered_dt = df_contract[
+            df_contract["startTime"].dt.strftime("%H:%M").isin(["00:59", "08:59", "16:59"])
+        ].copy()
+
+        # Reverse the order and calculate cumulated funding
+        filtered_dt = filtered_dt.iloc[::-1].copy()  # Reverse the order
+        filtered_dt["cumFunding"] = filtered_dt["fundingRate"].cumsum()  # Cumsum in reverse order
+        filtered_dt = filtered_dt.iloc[::-1].copy()  # Reverse back to original order
+
+        # Cumulated funding trace
+        cum_funding_trace = go.Scatter(
+            x=filtered_dt["startTime"],
+            y=filtered_dt["cumFunding"],
+            name="Cumulated Funding",
+            marker={"color": "red"},
+        )
+
+        return funding_trace, cum_funding_trace
+
+    def sub_gap(self, merged_df: pd.DataFrame) -> tuple[go.Scatter, go.Scatter]:
+        """Draw the gap between two products."""
+        # Calculate and add the difference trace
+        diffCalc = 100 - merged_df["closePrice_long"] * 100 / merged_df["closePrice_short"]
+        diff_graph = go.Scatter(
+            x=merged_df["startTime"],
+            y=diffCalc,
+            name="Coefficient of difference",
+            marker={"color": "blue"},
+        )
+
+        # Trendline of difference trace
+        trendline = go.Scatter(
+            x=merged_df["startTime"],
+            y=diffCalc.rolling(window=150).mean(),
+            name="Trendline",
+            marker={"color": "black"},
+        )
+
+        return diff_graph, trendline
+
+    def plot_compare(  # noqa: PLR0913
         self,
         longContract: str,
         shortContract: str,
         lowerlimit: str = "2024-01-01 00:00",
         upperlimit: str = "2026-01-01 00:00",
+        draw_gap: bool = True,
+        draw_funding: bool = True,
     ) -> go.Figure:
         """Compare two datasets in a candlestick chart.
 
@@ -102,6 +166,8 @@ class Simulator:
             shortContract (str): File containing the short dataset
             lowerlimit (str): The lower bound date to filter the data (format: YYYY-MM-DD HH:MM)
             upperlimit (str): The upper bound date to filter the data (format: YYYY-MM-DD HH:MM)
+            draw_gap (bool): If True, will draw the gap
+            draw_funding (bool): If True, will draw the cumulated funding
 
         Returns:
             go.Figure: The plotly figure
@@ -109,8 +175,8 @@ class Simulator:
         """
         # Get DataFrames for Long and Short datasets
         try:
-            _, dfLong = self.plot_candles(longContract, lowerlimit=lowerlimit, upperlimit=upperlimit)
-            _, dfShort = self.plot_candles(shortContract, lowerlimit=lowerlimit, upperlimit=upperlimit)
+            dfLong = self.to_graph(longContract, lowerlimit=lowerlimit, upperlimit=upperlimit, onlyData=True)
+            dfShort = self.to_graph(shortContract, lowerlimit=lowerlimit, upperlimit=upperlimit, onlyData=True)
         except Exception:
             print("One limit is incorrect.")
             raise
@@ -119,7 +185,9 @@ class Simulator:
         merged_df = dfLong.merge(dfShort, suffixes=("_long", "_short"), how="inner", on="startTime")
 
         # Initialize figure
-        fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.02)
+        fig = make_subplots(
+            rows=1 + int(draw_gap) + int(draw_funding), cols=1, shared_xaxes=True, vertical_spacing=0.02
+        )
 
         # Add Long position candlestick with specific color
         fig.add_trace(
@@ -160,24 +228,15 @@ class Simulator:
         fig.data[1].increasing.fillcolor = "red"
         fig.data[1].increasing.line.color = "red"
 
-        # Calculate and add the difference trace
-        diffCalc = 100 - merged_df["closePrice_long"] * 100 / merged_df["closePrice_short"]
-        diff_graph = go.Scatter(
-            x=merged_df["startTime"],
-            y=diffCalc,
-            name="Coefficient of difference",
-            marker={"color": "blue"},
-        )
-        fig.add_trace(diff_graph, row=2, col=1)
+        if draw_gap:
+            diff_graph, trendline = self.sub_gap(merged_df=merged_df)
+            fig.add_trace(diff_graph, row=2, col=1)
+            fig.add_trace(trendline, row=2, col=1)
 
-        # Trendline of difference trace
-        trendline = go.Scatter(
-            x=merged_df["startTime"],
-            y=diffCalc.rolling(window=150).mean(),
-            name="Trendline",
-            marker={"color": "black"},
-        )
-        fig.add_trace(trendline, row=2, col=1)
+        if draw_funding:
+            funding_trace, cum_funding_trace = self.sub_fundings(df_contract=merged_df)
+            fig.add_trace(funding_trace, row=3, col=1)
+            fig.add_trace(cum_funding_trace, row=3, col=1)
 
         # Final layout updates
         fig.update_layout(
